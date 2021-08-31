f73a66f0520c3e0e45205ca06252c1e06968e72e
Thalita Rosa de Jesus <66748284+thality@users.noreply.github.com>
2021-08-31 20:34:59 -0300

    Update vetor_int.java

--- vetor_int.java
+++ vetor_int.java
@@ -1,17 +1,23 @@
 /******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
+Thalita Rosa
 *******************************************************************************/
 
 public class Main
 {
 	public static void main(String[] args) {
 		int[] vetorzao = new int[10];
-		
-		for(i=0; i<vetorzao.lenght; i++){
+		int i;
+		vetorzao[0] = 1;
+		vetorzao[1] = 1;
+		vetorzao[2] = 1;
+		vetorzao[3] = 1;
+		vetorzao[4] = 1;
+		vetorzao[5] = 1;
+		vetorzao[6] = 1;
+		vetorzao[7] = 1;
+		vetorzao[8] = 1;
+		vetorzao[9] = 1;
+		for(i=0; i<10; i++){
 		    System.out.println(vetorzao[i]);
 		}
 		
